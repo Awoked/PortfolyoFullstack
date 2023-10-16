7329ffbf732eef7799b5bba98b151cde6cb25ca1
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-10-16 21:00:22 +0300

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentailsProvider from "next-auth/providers/credentials";
 import prisma from "../../../../../prisma/client";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         CredentailsProvider({
             name: "creds",
