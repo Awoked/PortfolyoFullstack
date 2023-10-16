9af5278ce0718ce72388acbb18faa662d6d0372b
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-10-16 21:25:46 +0300

    update

--- src/app/components/HeroSection/index.jsx
+++ src/app/components/HeroSection/index.jsx
@@ -6,7 +6,7 @@ import { Bounce, Circ, Elastic, Expo, Power3, Power4, gsap } from 'gsap';
 import { BsCaretDown } from "react-icons/bs";
 import styles from "./hero.module.css";
 import Image from 'next/image';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 
 const HeroSection = () => {
 
@@ -57,10 +57,13 @@ const HeroSection = () => {
     const ScrollDown = () => {
         window.scrollTo({ top: window.innerHeight, behavior: 'smooth' });
     }
-
+    const {data, status} = useSession();
     return (
         <>
-
+        <br />
+        <br />
+        <br />
+{JSON.stringify(data)}
             <section
                 className={`hero-section p-6 max-md:py-14 md:p-12 xl:p-24 relative h-[100svh] overflow-hidden text-black ${styles.background}`}
             >
