46a86bc742dc98e254227c8411cc465755e9054d
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-10-16 11:28:32 +0300

    update

--- src/app/api/sections/route.ts
+++ src/app/api/sections/route.ts
@@ -1,11 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-// import prisma from "../../../../prisma/client";
+import prisma from "../../../../prisma/client";
 
 
 export async function GET(req: NextRequest) {
-//   const data = await prisma.sectionData.findMany();
-//   console.log("data", data);
-
-const data = [{asd: true}];
+  const data = await prisma.sectionData.findMany();
+  console.log("data", data);
   return NextResponse.json(data);
 }
