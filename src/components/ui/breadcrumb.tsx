c5b242b7190d95b17c38519018557e78df96cfb6
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-10-20 14:30:30 +0300

    update

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -5,22 +5,24 @@ import React from 'react'
 
 const Breadcrumb = () => {
     const paths = usePathname()
+    console.log('paths', paths)
     const pathNames = paths.split('/').filter(path => path)
     return (
         <div>
             <ul className='flex gap-4 items-center'>
                 {
                     pathNames.map((link, index) => {
-                        let href = `/${pathNames.slice(0, index + 1).join('/')}`
-                        let itemLink = link
+                        let href = `/${pathNames.slice(0, index + 1).join('/')}`;
+                        let active = paths === href;
+                        let itemLink = link;
                         return (
                             <React.Fragment key={index}>
-                                <li >
+                                <li className={`${active ? "font-bold" : "font-medium"}`}>
                                     <Link href={href}>{itemLink}</Link>
                                 </li>
-                                <li>
-                                    {pathNames.length !== index + 1 && "/"}
-                                </li>
+                                {pathNames.length !== index + 1 && (
+                                    <li>/</li>
+                                )}
                             </React.Fragment>
                         )
                     })
