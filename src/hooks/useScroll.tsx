6b58a00c3b1e4ea1ae62d745380f3f69accfad79
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2023-11-10 00:04:10 +0300

    update

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -2,15 +2,13 @@
 import React, { useEffect, useState } from 'react'
 
 const useScroll = () => {
-    const [scrollCount, setScrollCount] = useState<number>(window.scrollY);
+    const [scrollCount, setScrollCount] = useState<number>(0);
 
     function scrollHandler() {
         setScrollCount(window.scrollY)
     }
 
     useEffect(() => {
-
-
         window.addEventListener("scroll", scrollHandler)
 
         return () => window.removeEventListener("scroll", scrollHandler)
